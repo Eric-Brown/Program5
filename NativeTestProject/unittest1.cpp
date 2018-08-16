900585493fef19fa4b1fdf84e61ec83eab93b2fb
Eric Brown <alexander7hegreat@gmail.com>
2018-08-16 15:42:47 -0600

    Another Commit
    Been a while since i worked on this. Not sure what's going on, but i just want to commit this before i format the hard drive.

---
+++ NativeTestProject/unittest1.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NativeTestProject
+{		
+	TEST_CLASS(TestProgram5)
+	{
+	public:
+		
+		TEST_METHOD(TestCrossReference)
+		{
+			// TODO: Your test code here
+
+		}
+
+	};
+}
